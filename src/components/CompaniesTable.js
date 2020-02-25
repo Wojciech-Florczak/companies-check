349a64d5867c1f4fae70739860ca4e629896ea6b
wojciech-florczak <florczak@wojciech.dev>
2020-02-25 22:51:33 +0100

    Feat: add helper function for summing up, CompaniesTable now shows total, average and last month incomes, create simple search function to use later

--- src/components/CompaniesTable.js
+++ src/components/CompaniesTable.js
@@ -1,43 +1,111 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { sortASC } from "../helpers";
+import { sortASC, sumUp } from "../helpers";
 import Pagination from "./Pagination";
 
 export default function CompaniesTable() {
   const [companies, setCompanies] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [itemsPerPage, setItemsPerPage] = useState(20);
+  const [currentCompanies, setCurrentCompanies] = useState([]);
 
-  async function getData() {
+  async function fetchData() {
     let res = await axios.get("https://recruitment.hal.skygate.io/companies");
     let sorted = sortASC(res.data, "id");
     setCompanies(sorted);
   }
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    getFullData();
+  }, [currentPage, companies]);
+
   const lastItemIndex = currentPage * itemsPerPage;
   const firstItemIndex = lastItemIndex - itemsPerPage;
 
-  const currentCompanies = companies.slice(firstItemIndex, lastItemIndex);
+  function getLastMonth() {
+    let date = new Date();
+    let currentYear = date.getFullYear();
+    let lastMonth = date
+      .getMonth()
+      .toString()
+      .padStart(2, "0");
+    let result = `${currentYear}-${lastMonth}`;
+    return result;
+  }
+
+  /* Make request only for companies that are being displayed to reduce number of requests */
+  async function getFullData() {
+    // TODO Modify this function to get full data of all companies when column header is clicked or search input has changed
+    // TODO Implement requests caching
+    const current = companies.slice(firstItemIndex, lastItemIndex);
+    const currentCompaniesFullInfo = current.map(async company => {
+      const res = await axios.get(
+        `https://recruitment.hal.skygate.io/incomes/${company.id}`
+      );
+      const incomes = res.data.incomes;
+
+      const totalIncome = sumUp(incomes);
+      const averageIncome = parseFloat(
+        (totalIncome / incomes.length).toFixed(2)
+      );
+      const lastMonthIncome = sumUp(
+        incomes.filter(income => income.date.startsWith(getLastMonth()))
+      );
+      return {
+        ...company,
+        totalIncome,
+        averageIncome,
+        lastMonthIncome
+      };
+    });
+    const results = await Promise.all(currentCompaniesFullInfo);
+    setCurrentCompanies(results);
+  }
+
   const renderCompanies = currentCompanies.map((company, i) => {
+    const {
+      id,
+      name,
+      city,
+      totalIncome,
+      averageIncome,
+      lastMonthIncome
+    } = company;
     return (
       <tr key={i}>
-        <td>{company.id}</td>
-        <td>{company.name}</td>
-        <td>{company.city}</td>
+        <td>{id}</td>
+        <td>{name}</td>
+        <td>{city}</td>
+        <td>{totalIncome.toFixed(2)}</td>
+        <td>{averageIncome.toFixed(2)}</td>
+        <td>{lastMonthIncome.toFixed(2)}</td>
       </tr>
     );
   });
 
+  function simpleSearch(term) {
+    return companies.filter(company => {
+      if (company.name.toLowerCase().includes(term)) {
+        return company;
+      } else if (company.city.toLowerCase().includes(term)) {
+        return company;
+      } else if (company.id.toString().includes(term)) {
+        return company;
+      }
+    });
+  }
+  console.log(simpleSearch("port"));
+
+  console.log(currentCompanies);
+
+  // handle sorting
   const handleClick = val => {
     setCompanies([...sortASC(companies, val)]);
-    setCurrentPage(1)
+    setCurrentPage(1);
   };
-
-  // on mount
-  useEffect(() => {
-    getData();
-  }, []);
-
-  console.dir(companies);
 
   return (
     <>
@@ -59,11 +127,24 @@ export default function CompaniesTable() {
             <th scope="col">
               <button onClick={() => handleClick("city")}>City</button>
             </th>
+            <th scope="col">
+              <button onClick={() => handleClick("totalIncome")}>
+                Total Income
+              </button>
+            </th>
+            <th scope="col">
+              <button onClick={() => handleClick("averageIncome")}>
+                Average Income
+              </button>
+            </th>
+            <th scope="col">
+              <button onClick={() => handleClick("lastMonthIncome")}>
+                Last Month Income
+              </button>
+            </th>
           </tr>
         </thead>
-        <tbody>
-          {renderCompanies}
-        </tbody>
+        <tbody>{renderCompanies}</tbody>
       </table>
     </>
   );
--- src/helpers.js
+++ src/helpers.js
@@ -1,7 +1,14 @@
-export function sortASC(arr, key){
-  return arr.sort((a, b) => a[key] < b[key] ? -1 : (a[key] > b[key] ? 1 : 0))
+export function sortASC(arr, key) {
+  return arr.sort((a, b) => (a[key] < b[key] ? -1 : a[key] > b[key] ? 1 : 0));
 }
 
-export function sortDESC(arr, key){
-  return arr.sort((a, b) => a[key] > b[key] ? -1 : (a[key] < b[key] ? 1 : 0))
+export function sortDESC(arr, key) {
+  return arr.sort((a, b) => (a[key] > b[key] ? -1 : a[key] < b[key] ? 1 : 0));
+}
+
+export function sumUp(arr) {
+  return arr.reduce((acc, currVal) => {
+    currVal = parseFloat(currVal.value);
+    return parseFloat((acc + currVal).toFixed(10));
+  }, 0);
 }
